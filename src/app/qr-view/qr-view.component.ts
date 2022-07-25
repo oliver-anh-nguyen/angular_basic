372d7e2af1278e007c8b07384ed87504f850a5ec
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2022-07-24 21:09:41 -0500

    [Angular] update param url qrcode

--- src/app/qr-view/qr-view.component.ts
+++ src/app/qr-view/qr-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-qr-view',
@@ -7,16 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrViewComponent implements OnInit {
   public title = 'QR';
-  public qrInfo = 'https://zingnews.vn/';
   public width = 300;
-  constructor() { }
+  public baseInfo = 'https://codelaghien.club/covid19';
+  public qrInfo = this.baseInfo;
+  public name = '';
+  public age = 0;
+
+  constructor(route: ActivatedRoute) {
+    route.queryParams.subscribe((v) => {
+      console.log('queryParams=', v);
+      // if (v && v.data) {
+      //   const data = v.data;
+      //   console.log('queryParams data =', data);
+      //   console.log('name =', data.name);
+      //   console.log('age =', data.age);
+      //
+      //   const obj = JSON.parse(data);
+      //   console.log('queryParams obj =', obj);
+      //   console.log('name =', obj.name);
+      //   console.log('age =', obj.age);
+      // }
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  public change(event: any): void {
-    console.log(event.target.value);
+  public changeBase(event: any): void {
+    this.baseInfo = event.target.value;
     this.qrInfo = event.target.value;
+    this.combine();
+  }
+
+  public changeName(event: any): void {
+    this.name = event.target.value;
+    this.combine();
+  }
+
+  public changeAge(event: any): void {
+    this.age = +event.target.value;
+    this.combine();
+  }
+
+  private combine(): void {
+    const data = JSON.stringify({ name: this.name, age: this.age });
+    this.qrInfo = this.baseInfo + '?data=' + data;
+    console.log('qrInfo=', this.qrInfo);
   }
 
 }
